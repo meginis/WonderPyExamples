344af0d1af26afdee731928ab58779602a0ed113
Meginis <lancevickers@gmail.com>
2019-08-05 12:02:22 -0500

    Update logan.py

--- dev/logan.py
+++ dev/logan.py
@@ -23,7 +23,24 @@ app = web.Application()
 # Binds our Socket.IO server to our Web App
 # instance
 sio.attach(app)
+def index(request):
+    with open('index.html') as f:
+        return web.Response(text=f.read(), content_type='text/html')
 
+# If we wanted to create a new websocket endpoint,
+# use this decorator, passing in the name of the
+# event we wish to listen out for
+@sio.on('message')
+def print_message(sid, message):
+    # When we receive a new event of type
+    # 'message' through a socket.io connection
+    # we print the socket ID and the message
+    print("Socket ID: " , sid)
+    print(message)
+
+# We bind our aiohttp endpoint to our app
+# router
+app.router.add_get('/', index)
 
 class Direction(object):
     LEFT = 0
@@ -59,25 +76,6 @@ TWITTER_ACCESS_TOKEN_SECRET = ""
 # populate this list with the Twitter accounts you want to listen to
 TWITTER_USERS = [""]
 TWITTER_LANG = ["en"]
-
-async def index(request):
-    with open('index.html') as f:
-        return web.Response(text=f.read(), content_type='text/html')
-
-# If we wanted to create a new websocket endpoint,
-# use this decorator, passing in the name of the
-# event we wish to listen out for
-@sio.on('message')
-async def print_message(sid, message):
-    # When we receive a new event of type
-    # 'message' through a socket.io connection
-    # we print the socket ID and the message
-    print("Socket ID: " , sid)
-    print(message)
-
-# We bind our aiohttp endpoint to our app
-# router
-app.router.add_get('/', index)
 
 def is_numeric(s):
     '''
