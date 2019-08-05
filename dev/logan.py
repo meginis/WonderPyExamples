d3ca29341cea126f3a52abe48dbc4ca1177ed460
Lance <lance@decisivedesign.com>
2019-08-05 16:32:45 -0500

    Connected

--- dev/logan.py
+++ dev/logan.py
@@ -53,21 +53,8 @@ TWITTER_ACCESS_TOKEN_SECRET = ""
 # populate this list with the Twitter accounts you want to listen to
 TWITTER_USERS = [""]
 TWITTER_LANG = ["en"]
-sio = socketio.Server()
-app = socketio.WSGIApp(sio, static_files={
-   '/': {'content_type': 'text/html', 'filename': './index.html'}
-})
-@sio.event
-def connect(sid, environ):
-    print('connect ', sid)
 
-@sio.event
-def my_message(sid, data):
-    print('message ', data)
 
-@sio.event
-def disconnect(sid):
-    print('disconnect ', sid)
 def is_numeric(s):
     '''
     Determines if a string is numeric (including negative and decimal numbers)
@@ -106,9 +93,27 @@ class TwitterBot(object):
         self._async_thread1.start()
         self._async_thread2 = Thread(target=self.twitter_async)
         self._async_thread2.start()
+        self._async_thread3 = Thread(target=self.socketStartup)
+        self._async_thread3.start()
+        
+    def socketStartup(self):
+        sio = socketio.Server()
+        app = socketio.WSGIApp(sio, static_files={
+        '/': {'content_type': 'text/html', 'filename': './index.html'}
+        })
+        @sio.event
+        def connect(sid, environ):
+            print('connect ', sid)
 
+        @sio.event
+        def message(sid, data):
+            print('message ', data)
+
+        @sio.event
+        def disconnect(sid):
+            print('disconnect ', sid)
+        print('Starting up server')
         eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
-
     def action_listener_async(self):
         '''
         Performs actions placed on the action queue
