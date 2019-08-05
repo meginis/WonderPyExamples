4175ea5eebcd85e20cffbb3d42632e0521031da8
Meginis <lancevickers@gmail.com>
2019-08-05 13:46:04 -0500

    updates

--- dev/logan.py
+++ dev/logan.py
@@ -17,21 +17,7 @@ we are listening to the Twitter account @twitterBot:
 @twitterBot turn right 90
 '''
 
-sio = socketio.Server()
-app = socketio.WSGIApp(sio, static_files={
-    '/': {'content_type': 'text/html', 'filename': 'index.html'}
-})
-@sio.event
-def connect(sid, environ):
-    print('connect ', sid)
 
-@sio.event
-def my_message(sid, data):
-    print('message ', data)
-
-@sio.event
-def disconnect(sid):
-    print('disconnect ', sid)
 
 class Direction(object):
     LEFT = 0
@@ -86,7 +72,17 @@ class TwitterBot(object):
         self._action_queue = Queue()
         self._twitter_api = None
         self._robot = None
+    @sio.event
+    def connect(sid, environ):
+        print('connect ', sid)
 
+    @sio.event
+    def my_message(sid, data):
+        print('message ', data)
+
+    @sio.event
+    def disconnect(sid):
+        print('disconnect ', sid)
     def on_connect(self, robot):
         '''
         Kick off async threads
@@ -106,6 +102,11 @@ class TwitterBot(object):
         self._async_thread1.start()
         self._async_thread2 = Thread(target=self.twitter_async)
         self._async_thread2.start()
+        sio = socketio.Server()
+        app = socketio.WSGIApp(sio, static_files={
+            '/': {'content_type': 'text/html', 'filename': 'index.html'}
+        })
+        eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
 
     def action_listener_async(self):
         '''
@@ -261,4 +262,3 @@ class TwitterBot(object):
 
 if __name__ == "__main__":
     WonderPy.core.wwMain.start(TwitterBot())
-    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
